d93fd88fa04c6bd5580e863c66af91d457f8350a
ramu <ramu.yanamala1@gmail.com>
2023-04-03 15:56:42 +0530

    modified main method

--- Firstproject/src/main/java/com/example/demo/test/TestDemo.java
+++ Firstproject/src/main/java/com/example/demo/test/TestDemo.java
@@ -4,7 +4,9 @@ public class TestDemo {
 
 	public static void main(String[] args) {
 		int i=10;
-	
+	int a=25;
+	int c=i+a;
+	System.out.println(c);
 		System.out.println("this is sfirst program");
 
 	}
